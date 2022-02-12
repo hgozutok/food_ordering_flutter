2f7e36dad80ebb481a70c2a3f9a96b8fdf0ed359
Hugo <huseyin@gozutok.info>
2022-02-12 21:04:11 +0300

    api edited

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Constants {
   //urls for pages
-  static const String baseUrl = 'https://localhost:5001';
+  static const String baseUrl = 'https://h.gozutok.info/food';
   static const String products = baseUrl + '/products';
   static const String categories = baseUrl + '/products/categories';
   static const String login = baseUrl + '/api/user/token';
